b48ad29efff9380b494c2d580d4ac0f9a2d7648c
Arek Mamala <mamala132@gmail.com>
2020-03-23 13:48:36 +0000

    bomb scene updated

---
+++ MainPage.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Media.SpeechRecognition;
+using Windows.Storage;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace QuietMonday
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // create a speech recogniser object - listens
+        private SpeechRecognizer speechRecognizer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            speechRecognizer = new SpeechRecognizer();
+            speechRecognizer.Timeouts.BabbleTimeout = 
+                TimeSpan.FromSeconds(0);
+            speechRecognizer.Timeouts.InitialSilenceTimeout = 
+                TimeSpan.FromSeconds(5);
+            speechRecognizer.Timeouts.EndSilenceTimeout = 
+                TimeSpan.FromSeconds(0.5);
+
+            // load the grammar file here
+            var grammarFile = await StorageFile.GetFileFromApplicationUriAsync(
+                new Uri("ms-appx:///grammar.xml"));
+
+            // adds constraints to the speech recogniser
+            //speechRecognizer.Constraints.Add();
+            speechRecognizer.Constraints.Add(
+                new SpeechRecognitionGrammarFileConstraint(grammarFile));
+            var result = await speechRecognizer.CompileConstraintsAsync();
+            if (result.Status == SpeechRecognitionResultStatus.Success)
+            {
+                while (true)
+                {
+                    // listen in to the user
+                    SpeechRecognitionResult srr =
+                        await speechRecognizer.RecognizeAsync();
+                    // use the Semantic Interpretation engine
+                    // to get the commands
+                    string myCommand = "No Command";
+                    IReadOnlyList<string> values = null;
+
+                    if(srr.SemanticInterpretation.Properties.TryGetValue(
+                                            "command", out values) == true)
+                    {
+                        string ruleID = srr.RulePath[0];
+                        // do something to start a game - call a method
+                        myCommand = values.FirstOrDefault();
+                        switch (myCommand)
+                        {
+                            case "new":
+                                StartNewGame();
+                                break;
+                            case "quit":
+                                QuitGame();
+                                break;
+                        }
+                    }
+                    // see if there is a move command
+                    string player;
+                    int position;
+                    if (srr.SemanticInterpretation.Properties.TryGetValue(
+                                "Player", out values) == true)
+                    {
+                        player = values.FirstOrDefault();
+                        position =
+                            Convert.ToInt32(srr.SemanticInterpretation.Properties["Position"].Single());
+                        MakeAMove(player, position);
+                    }
+
+                }
+            }
+            else
+            {
+                var messageDialog = new Windows.UI.Popups.MessageDialog(
+                    "Could load grammar...",
+                    "Load error");
+                await messageDialog.ShowAsync();
+            }
+        }
+
+        private async void MakeAMove(string player, int position)
+        {
+            var messageDialog = new Windows.UI.Popups.MessageDialog(
+                "Move " + player + " to position " + position + "...", 
+                "Making A Move");
+            await messageDialog.ShowAsync();
+        }
+
+        private async void QuitGame()
+        {
+            var messageDialog = new Windows.UI.Popups.MessageDialog(
+                "Quitting Game, Not Saving, please wait...", "Quit Game");
+            await messageDialog.ShowAsync();
+        }
+
+        private async void StartNewGame()
+        {
+            var messageDialog = new Windows.UI.Popups.MessageDialog(
+                "Setting up a new game, please wait...","Starting New Game");
+            await messageDialog.ShowAsync();
+        }
+    }
+
+    }
---
+++ Project/Assets/Scripts/buttonHandler.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class buttonHandler : MonoBehaviour
+{
+    wires wire = new wires(); 
+    
+    [SerializeField]
+    GameObject openCode, closeCode;
+    public static bool isCodeAvailable = false;
+    
+    [SerializeField]
+    Text number;
+
+    [SerializeField]
+    Text countdownText;
+
+    float currentTime = 0f;
+    float startingTime = 200f; 
+
+    string randomNumber = "";
+
+    void Start(){
+
+        closeCode.SetActive (true);
+        openCode.SetActive (false);
+
+        currentTime = startingTime;
+    }
+
+    void Update(){
+
+        if(randomNumber == ""){
+            randomNumber = number.text;
+
+            Debug.Log (randomNumber+ "this is the text");
+        }
+
+        cutWire();
+
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("00");
+
+        if(currentTime <=0 ){
+            currentTime = 0;
+        }
+
+        if (currentTime<=10){
+           countdownText.color = Color.red;
+        }
+        
+    
+   
+    }
+
+    public void cutWire(){
+        // Text txt = transform.Find("Text").GetComponent<Text>();
+        // txt.text = text + number;   
+
+        switch (randomNumber) {
+            case "1":
+                // three wires 
+                // blue wire (last wire) to unlock 
+
+                //if the user says blue wire or presses the "b" button 
+                //therefore fail
+                if(Input.GetKeyDown(KeyCode.B)){
+                    //unlock the code
+                    isCodeAvailable = true;
+                    closeCode.SetActive (false);
+                    openCode.SetActive (true); 
+                    //Debug.Log ("working correctly");
+                }  
+                //if the user says any other wire or presses other button 
+                //therefore fail
+                else if(Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.G))
+                {
+                    // speed the timer
+                    Debug.Log ("timer speed up");
+                    this.currentTime -= 100;
+                    countdownText.color = Color.red;
+                    Debug.Log ("timer speed up "+ currentTime);
+                }else{
+                    //Debug.Log ("not working correctly");
+                }
+
+
+                break;
+            case "2":
+                //four wires 
+                //cut the first wire (white wire)
+                
+                 //if the user says white wire or presses the "w" button 
+                //therefore fail
+                if(Input.GetKeyDown(KeyCode.W)){
+                    //unlock the code
+                    isCodeAvailable = true;
+                    closeCode.SetActive (false);
+                    openCode.SetActive (true); 
+                    //Debug.Log ("working correctly");
+                }  
+                //if the user says any other wire or presses other button 
+                //therefore fail
+                else if(Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(KeyCode.G))
+                {
+                    // speed the timer
+                    this.currentTime -= 100;
+                    countdownText.color = Color.red;
+                    Debug.Log ("timer speed up "+ currentTime);
+
+                }else{
+                    //Debug.Log ("not working correctly");
+                }
+
+                Debug.Log (isCodeAvailable);
+                break;
+            case "3":
+                //five wires 
+                // cut the second wire (red wire)
+
+                 //if the user says red wire or presses the "r" button 
+                //therefore fail
+                if(Input.GetKeyDown(KeyCode.R)){
+                    //unlock the code
+                    isCodeAvailable = true;
+                    closeCode.SetActive (false);
+                    openCode.SetActive (true); 
+                    //Debug.Log ("working correctly");
+                }  
+                //if the user says any other wire or presses other button 
+                //therefore fail
+                else if(Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.G))
+                {
+                    // speed the timer
+                    this.currentTime -= 20 ;
+                    countdownText.color = Color.red;
+                    Debug.Log ("timer speed up "+ currentTime);
+                }else{
+                    //Debug.Log ("not working correctly");
+                }
+               // Debug.Log (isCodeAvailable);
+                break;
+            case "4":
+                // six wires 
+                // cut the fourth wire (blue wire)
+
+                 //if the user says blue wire or presses the "b" button 
+                //therefore fail
+                if(Input.GetKeyDown(KeyCode.B)){
+                    //unlock the code
+                    isCodeAvailable = true;
+                    closeCode.SetActive (false);
+                    openCode.SetActive (true); 
+                    //Debug.Log ("working correctly");
+                }  
+                //if the user says any other wire or presses other button 
+                //therefore fail
+                else if(Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.G))
+                {
+                    // speed the timer
+                    this.currentTime -= 100;
+                    countdownText.color = Color.red;
+                    Debug.Log ("timer speed up "+ currentTime);
+
+                }else{
+                    //Debug.Log ("not working correctly");
+                }
+                Debug.Log (isCodeAvailable);
+                break;
+        }
+    
+    }
+
+}
