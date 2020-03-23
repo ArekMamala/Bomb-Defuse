6e4b333b48858004e6baaecfbe536c6d6d48e261
Arek Mamala <mamala132@gmail.com>
2020-03-23 16:25:04 +0000

    update

---
+++ Project/Assets/Scripts/navigation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class navigation : MonoBehaviour
+{
+    public void sceneChange(string level){
+        SceneManager.LoadScene(level);
+        
+    }
+
+    
+}
