722478ac5fc11c5dd712c1c20785909da90bccdf
Sammar Tahir <G00347526@gmit.ie>
2020-03-29 14:37:53 +0100

    Can not get voice to work on linux

--- Project/Assets/VoiceMovement.cs
+++ Project/Assets/VoiceMovement.cs
@@ -9,10 +9,32 @@ public class VoiceMovement : MonoBehaviour
   private Dicitonary<string, Action> actions = new Dicitonary<string, Action>();
 
   void Start(){
-      actions.Add("Start", Start);
-      actions.Add("Quit", Quit);
-      actions.Add("Setting", Setting);
-      actions.Add("Start Game", Manual);
+    actions.Add("Start", StartGame);
+    actions.Add("Quit", Quit);
+    actions.Add("Setting", Setting);
+    actions.Add("Manual", Manual);
+
+    keywordRecognizer = new keywordRecognizer(actions.Keys.ToArray());
+    keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+  }
+
+  private void RecognizedSpeech(PhraseRecognizedEventArgs speech){
+    Debug.Log(speech.text);
+    actions[speech.text].Invoke();
+  }
+  
+  private void StartGame(){
+    // Code to here on windows 
+  }
+
+  private void Quit(){
+    // Code to here on windows 
+  }
+  private void Setting(){
+    // Code to here on windows 
+  }
+  private void Manual(){
+    // Code to here on windows 
   }
 
 }
