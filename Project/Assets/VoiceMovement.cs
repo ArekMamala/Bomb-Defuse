2a96df17ce4e4933f2c607ceb4b304d44ce93bca
Sammar Tahir <G00347526@gmit.ie>
2020-03-29 14:25:48 +0100

    Changing normal wii controls to voice

---
+++ Project/Assets/VoiceMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceMovement : MonoBehaviour
+{
+  private keywordRecognizer keywordRecognizer;
+  private Dicitonary<string, Action> actions = new Dicitonary<string, Action>();
+
+  void Start(){
+      actions.Add("Start", Start);
+      actions.Add("Quit", Quit);
+      actions.Add("Setting", Setting);
+      actions.Add("Start Game", Manual);
+  }
+
+}
