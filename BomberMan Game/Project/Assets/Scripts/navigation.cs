9b78c084a226d07ab86083bd0fb0785845bca401
Sammar Tahir <G00347526@gmit.ie>
2020-04-04 19:31:53 +0100

    cleaning git repo with working game

---
+++ BomberMan Game/Project/Assets/Scripts/navigation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+using UnityEngine.Windows.Speech;
+
+public class navigation : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pauseMenuUI;
+
+     //speech recognitiojn
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    public static bool paused = false;
+    public  bool escape = false;
+
+    public  string scene="";
+
+    void Start () {
+
+        //voice recognition
+        actions.Add("play", play);
+        actions.Add("quit", QuitGame);
+        actions.Add("menu", instructions);
+        actions.Add("stop", manual);        
+        actions.Add("home", home);
+        
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+        Debug.Log(actions);
+
+
+    }
+
+    void Update(){
+        //pauseGame();
+    }
+    
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech){
+			Debug.Log(speech.text);
+			actions[speech.text].Invoke();
+
+    }
+
+    void play(){
+        this.scene = "Bomb";
+        SceneManager.LoadScene("Bomb");
+    }
+    
+    void home(){
+        this.scene = "HomeMenu";
+        SceneManager.LoadScene("HomeMenu");
+    }
+
+    void instructions(){
+        this.scene = "Instructions";
+        SceneManager.LoadScene("Instructions");
+    }
+    void manual(){
+        this.escape = true;
+        pauseGame();
+    }
+   
+
+
+    public void sceneChange(string level){
+        SceneManager.LoadScene(level);
+        
+    }
+
+    public void QuitGame(){
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void pauseGame(){
+        if(Input.GetKeyDown(KeyCode.Escape) || (this.escape = true))
+        {
+            if(paused)
+            {
+                Resume();
+            }else{
+                Pause();
+            }
+            this.escape=false;
+        }
+    }
+
+    void Resume(){
+        pauseMenuUI.SetActive(false);
+        paused = false;
+    }
+
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        paused = true;
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,42 +1,7 @@
-## Insert Name Here
-One player takes the role of the bomb Defuser. All other players take the role of Experts who must use the Bomb Defusal Manual to guide the Defuser through the challenging task of defusing a bomb.
+## BomberMan
 
-### Rules
-#### On the Subject of Wires
-Wires are the lifeblood of electronics! Wait, no, electricity is the lifeblood.
-<br>
-Wires are more like the arteries. The veins? No matter…
+This game is all about skill and how smart you and your friends are when is comes to working under stressful situations. You must use the Manual to defuse the bomb before the time runs out. 
 
- ###### A wire module can have 3-6 wires on it.
-Only the one correct wire needs to be cut to disarm themodule.
-<br>
-Wire ordering begins with the first on the top.
-<br>
-**3 wires:**
+### How to run the game
 
-* If there are no red wires, cut the second wire.
-* Otherwise, if the last wire is white, cut the last wire.
-* Otherwise, if there is more than one blue wire, cut the last blue wire.
-* Otherwise, cut the last wire.
-<br>
-**4 wires:**
-
-* If there is more than one red wire and the last digit of the serial number is odd, cut the last red wire.
-* Otherwise, if the last wire is yellow and there are no red wires, cut the first wire.
-* Otherwise, if there is exactly one blue wire, cut the first wire.
-* Otherwise, if there is more than one yellow wire, cut the last wire.
-* Otherwise, cut the second wire.
-<br>
-**5 wires:**
-
-* If the last wire is black and the last digit of the serial number is odd, cut the fourth wire.
-* Otherwise, if there is exactly one red wire and there is more than one yellow wire, cut the first wire.
-* Otherwise, if there are no black wires, cut the second wire.
-* Otherwise, cut the first wire.
-<br>
-**6 wires:**
-
-* If there are no yellow wires and the last digit of the serial number is odd, cut the third wire.
-* Otherwise, if there is exactly one yellow wire and there is more than one white wire, cut the fourth wire.
-* Otherwise, if there are no red wires, cut the last wire.
-* Otherwise, cut the fourth wire.
+ ###### Game by <br> Arkadiusz Mamala <br> Sammar Tahir
